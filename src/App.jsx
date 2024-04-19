ee9644c46ff965521951ebc3fe1ee5071498ad48
Petr Jiruše <petrjiruse@seznam.cz>
2024-04-19 15:39:53 +0200

    Commit part 7/2

--- src/App.jsx
+++ src/App.jsx
@@ -4,56 +4,6 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import ReactDOM from "react-dom/client";
 
-class Clock extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = { date: new Date() };
-    }
-
-    componentDidMount() {
-        this.timerID = setInterval(
-            () => this.tick(),
-            1000
-        );
-    }
-
-    componentWillUnmount() {
-        clearInterval(this.timerID);
-    }
-
-    tick() {
-        this.setState({
-            date: new Date()
-        });
-    }
-
-    render() {
-        return (
-            <div>
-                <h1>Hello, world!</h1>
-                <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
-            </div>
-        );
-    }
-}
-
-function Welcome(props) {
-    return <h1>Hello, {props.name}!</h1>;
-}
-
-function Form() {
-    function handleSubmit(e) {
-        e.preventDefault();
-        console.log('You clicked submit.');
-    }
-
-    return (
-        <form onSubmit={handleSubmit}>
-            <button type="submit">Submit</button>
-        </form>
-    );
-}
-
 function UserGreeting(props) {
     return <h1>Welcome back!</h1>;
 }
@@ -70,25 +20,43 @@ function Greeting(props) {
     return <GuestGreeting />;
 }
 
+function LoginButton(props) {
+    return (
+        <button onClick={props.onClick}>
+            Login
+        </button>
+    );
+}
+
+function LogoutButton(props) {
+    return (
+        <button onClick={props.onClick}>
+            Logout
+        </button>
+    );
+}
+
 function App() {
     const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-    function handleLoginClick() {
+    const handleLoginClick = () => {
         setIsLoggedIn(true);
-    }
+    };
 
-    function handleLogoutClick() {
+    const handleLogoutClick = () => {
         setIsLoggedIn(false);
-    }
+    };
+
+    let button = isLoggedIn ? (
+        <LogoutButton onClick={handleLogoutClick} />
+    ) : (
+        <LoginButton onClick={handleLoginClick} />
+    );
 
     return (
         <div>
-            <Welcome name="Peter"/>
-            <button onClick={handleLoginClick}>Log in</button>
-            <button onClick={handleLogoutClick}>Log out</button>
             <Greeting isLoggedIn={isLoggedIn} />
-            <Clock />
-            <Form />
+            {button}
         </div>
     );
 }
